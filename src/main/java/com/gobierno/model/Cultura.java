6711b860831056b5bb395aecdc6754a420867d1a
luis09u <luisaj020104@gmail.com>
2026-07-14 17:22:58 -0500

    termino del bai y dss reparación

--- src/main/java/com/gobierno/model/Cultura.java
+++ src/main/java/com/gobierno/model/Cultura.java
@@ -59,5 +59,46 @@ public class Cultura {
         this.referenciaEspecifica = referenciaEspecifica;
     }
 
-    // Getters y Setters...
+
+    /*
+    public String getDocumentacionRelacionada() {
+        return documentacionRelacionada;
+    }
+
+    public void setDocumentacionRelacionada(String documentacionRelacionada) {
+        this.documentacionRelacionada = documentacionRelacionada;
+    }
+
+   public String getElementoCultural() {
+        return elementoCultural;
+    }
+
+    public void setElementoCultural(String elementoCultural) {
+        this.elementoCultural = elementoCultural;
+    }
+
+    public Long getIdCultura() {
+        return idCultura;
+    }
+
+    public void setIdCultura(Long idCultura) {
+        this.idCultura = idCultura;
+    }
+
+    public ProcesoCobit getProceso() {
+        return proceso;
+    }
+
+    public void setProceso(ProcesoCobit proceso) {
+        this.proceso = proceso;
+    }
+
+    public String getReferenciaEspecifica() {
+        return referenciaEspecifica;
+    }
+
+    public void setReferenciaEspecifica(String referenciaEspecifica) {
+        this.referenciaEspecifica = referenciaEspecifica;
+    }*/
+
 }
